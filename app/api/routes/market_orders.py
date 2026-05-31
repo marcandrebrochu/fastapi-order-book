73b581658ea9583048a91ffc5849b14a3e676add
Marc-André Brochu <marcandrebrochu@fastmail.com>
2026-05-31 13:00:46 -0400

    Add slippage query param

--- app/api/routes/market_orders.py
+++ app/api/routes/market_orders.py
@@ -1,6 +1,6 @@
-from typing import Any
+from typing import Annotated, Any
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 from app.models import ExecutedMarketOrderPublic, MarketOrderCreate
 
@@ -16,5 +16,16 @@ router = APIRouter(
     summary="Place a market order",
     response_model=ExecutedMarketOrderPublic,
 )
-def place_market_order(order_in: MarketOrderCreate) -> Any:
+def place_market_order(
+    order_in: MarketOrderCreate,
+    max_slippage: Annotated[float | None, Query(
+        ge=0.0,
+        le=100.0,
+        description=(
+            "Maximum slippage accepted, as a percentage. "
+            "If not specified, the order will proceed without a slippage limit, "
+            "which could incur significant losses."
+        ),
+    )] = None,
+) -> Any:
     pass
